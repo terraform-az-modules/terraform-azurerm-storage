1d81c10d66500363b0e72e524170fb839f87ef16
Abhinav Singh Shekhawat <abhinav.shekhawat@clouddrove.com>
2026-05-28 15:07:23 +0530

    fix: updated rule name for mgmt policy (#68)
    * fix: updated rule name for mgmt policy
    
    * fix: gemini comments

--- main.tf
+++ main.tf
@@ -341,7 +341,7 @@ resource "azurerm_storage_management_policy" "lifecycle_management" {
     for_each = var.management_policy
     iterator = it
     content {
-      name    = "rule${it.key}"
+      name    = coalesce(it.value.name, "rule${it.key}")
       enabled = true
       filters {
         prefix_match = it.value.prefix_match
@@ -366,6 +366,11 @@ resource "azurerm_storage_management_policy" "lifecycle_management" {
           delete_after_days_since_creation_greater_than                  = it.value.snapshot_delete_after_days
           tier_to_archive_after_days_since_last_tier_change_greater_than = it.value.tier_to_archive_after_days_since_last_tier_change_greater_than
           tier_to_cold_after_days_since_creation_greater_than            = it.value.tier_to_cold_after_days_since_creation_greater_than
+        }
+        version {
+          change_tier_to_archive_after_days_since_creation = it.value.change_tier_to_archive_after_days_since_creation
+          change_tier_to_cool_after_days_since_creation    = it.value.change_tier_to_cool_after_days_since_creation
+          delete_after_days_since_creation                 = it.value.delete_after_days_since_creation
         }
       }
     }
