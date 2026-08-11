8667f6ca91216a90498cfdca4aa43f5e552ad4ce
NotMaharshi <maharshi.bhatnagar@clouddrove.com>
2026-08-11 18:11:06 +0530

    fix: updated diagnostics for storage-nic

--- main.tf
+++ main.tf
@@ -458,10 +458,10 @@ resource "azurerm_monitor_diagnostic_setting" "datastorage" {
 ##-----------------------------------------------------------------------------
 ## Monitor Diagnostic Setting - Create diagnostic setting for storage-nic.
 ##-----------------------------------------------------------------------------
-resource "azurerm_monitor_diagnostic_setting" "storage-nic" {
+resource "azurerm_monitor_diagnostic_setting" "storage_nic" {
   depends_on                     = [azurerm_private_endpoint.pep]
   count                          = local.create_monitor_diagnostic_nic ? 1 : 0
-  name                           = var.resource_position_prefix ? format("mds-%s", local.name) : format("%s-mds", local.name)
+  name                           = var.resource_position_prefix ? format("nic-diag-st-%s", local.name) : format("%s-nic-diag-st", local.name)
   target_resource_id             = element(azurerm_private_endpoint.pep[count.index].network_interface[*].id, count.index)
   storage_account_id             = var.storage_account_id
   eventhub_name                  = var.eventhub_name
@@ -469,7 +469,7 @@ resource "azurerm_monitor_diagnostic_setting" "storage-nic" {
   log_analytics_workspace_id     = var.log_analytics_workspace_id
   log_analytics_destination_type = var.log_analytics_destination_type
   dynamic "enabled_metric" {
-    for_each = var.metrics
+    for_each = var.nic_metrics
     content {
       category = enabled_metric.value
     }
